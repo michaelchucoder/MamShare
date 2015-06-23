dc80bbe4bb6b75c500195b8e06c360dc4bfc2653
Michael <mrcoder@qq.com>
2015-06-23 16:09:22 +0800

    [Feature] SettingActivity and UserProfileActivity

---
+++ app/src/main/java/com/babyspace/mamshare/app/activity/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.babyspace.mamshare.app.activity;
+
+import android.os.Bundle;
+
+import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseActivity;
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+}
