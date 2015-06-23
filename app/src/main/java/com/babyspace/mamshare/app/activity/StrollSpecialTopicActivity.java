2af46a16f14b6189c3a77c8d209a925459f8ae40
Michael <mrcoder@qq.com>
2015-06-23 17:46:36 +0800

    [Feature] StrollSpecialTopicActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/StrollSpecialTopicActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/StrollSpecialTopicActivity.java
@@ -1,13 +1,10 @@
 package com.babyspace.mamshare.app.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseActivity;
 
-public class StrollSpecialTopicActivity extends AppCompatActivity {
+public class StrollSpecialTopicActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +12,4 @@ public class StrollSpecialTopicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stroll_special_topic);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_stroll_special_topic, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
