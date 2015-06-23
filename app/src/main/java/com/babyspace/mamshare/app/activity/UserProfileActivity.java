d27f314232e5cc6966114bda1bb59e9bf0afaffc
Michael <mrcoder@qq.com>
2015-06-23 16:09:51 +0800

    [Feature] SettingActivity and UserProfileActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/UserProfileActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/UserProfileActivity.java
@@ -2,8 +2,6 @@ package com.babyspace.mamshare.app.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.babyspace.mamshare.R;
 
@@ -15,25 +13,4 @@ public class UserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user_profile, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
