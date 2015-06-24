c6abaf64d475527e91da445c85a357e476473270
Michael <mrcoder@qq.com>
2015-06-24 16:23:43 +0800

    [UI] StrollFancyCoverFlowAdapter

--- app/src/main/java/com/babyspace/mamshare/app/activity/MamStrollActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/MamStrollActivity.java
@@ -60,8 +60,8 @@ public class MamStrollActivity extends BaseActivity {
         private final int[] images;
 
         public StrollFancyCoverFlowAdapter(int[] images) {
-            this.images=images;
-            
+            this.images = images;
+
         }
 
         // =============================================================================
