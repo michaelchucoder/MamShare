7d80ec101913f4ba86a05d57d5abde4644a8c229
Michael <mrcoder@qq.com>
2015-06-24 19:47:11 +0800

    [Feature] HomeActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/HomeActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/HomeActivity.java
@@ -9,58 +9,58 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.babyspace.mamshare.R;
-import com.babyspace.mamshare.app.fragment.DiscoverGuidanceFragment;
-import com.babyspace.mamshare.app.fragment.DiscoverLabelFragment;
-import com.babyspace.mamshare.app.fragment.DiscoverShareFragment;
+import com.babyspace.mamshare.app.fragment.MamSpecialTopicFragment;
+import com.babyspace.mamshare.app.fragment.MamDiscoverFragment;
+import com.babyspace.mamshare.app.fragment.MamUserCenterFragment;
 import com.babyspace.mamshare.basement.BaseActivity;
 
 
 public class HomeActivity extends BaseActivity implements View.OnClickListener {
 
-    private DiscoverGuidanceFragment discoverGuidanceFragment;
+    private MamSpecialTopicFragment specialTopicFragment;
 
-    private DiscoverLabelFragment discoverLabelFragment;
+    private MamDiscoverFragment discoverFragment;
 
-    private DiscoverShareFragment discoverShareFragment;
-
-
-    private View discoverGuidanceLayout;
+    private MamUserCenterFragment userCenterFragment;
 
 
-    private View discoverLabelLayout;
+    private View specialTopicLayout;
 
 
-    private View discoverShareLayout;
+    private View discoverLayout;
+
+
+    private View userCenterLayout;
 
     /**
      * 在Tab布局上显示攻略的控件
      */
-    private ImageView discoverGuidanceImage;
+    private ImageView specialTopicImage;
 
     /**
      * 在Tab布局上显示标签的控件
      */
-    private ImageView discoverLabelImage;
+    private ImageView discoverImage;
 
     /**
      * 在Tab布局上显示妈妈说图标的控件
      */
-    private ImageView discoverShareImage;
+    private ImageView userCenterImage;
 
     /**
      * 在Tab布局上显示消息标题的控件
      */
-    private TextView discoverGuidanceText;
+    private TextView specialTopicText;
 
     /**
      * 在Tab布局上显示联系人标题的控件
      */
-    private TextView discoverLabelText;
+    private TextView discoverText;
 
     /**
      * 在Tab布局上显示设置标题的控件
      */
-    private TextView discoverShareText;
+    private TextView userCenterText;
 
     /**
      * 用于对Fragment进行管理
@@ -83,32 +83,32 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
      * 在这里获取到每个需要用到的控件的实例，并给它们设置好必要的点击事件。
      */
     private void initViews() {
-        discoverGuidanceLayout = findViewById(R.id.discoverGuidance_layout);
-        discoverLabelLayout = findViewById(R.id.discoverLabel_layout);
-        discoverShareLayout = findViewById(R.id.discoverShare_layout);
-        discoverGuidanceImage = (ImageView) findViewById(R.id.discoverGuidance_image);
-        discoverLabelImage = (ImageView) findViewById(R.id.discoverLabel_image);
-        discoverShareImage = (ImageView) findViewById(R.id.discoverShare_image);
-        discoverGuidanceText = (TextView) findViewById(R.id.discoverGuidance_text);
-        discoverLabelText = (TextView) findViewById(R.id.discoverLabel_text);
-        discoverShareText = (TextView) findViewById(R.id.discoverShare_text);
-        discoverGuidanceLayout.setOnClickListener(this);
-        discoverLabelLayout.setOnClickListener(this);
-        discoverShareLayout.setOnClickListener(this);
+        specialTopicLayout = findViewById(R.id.specialTopic_layout);
+        discoverLayout = findViewById(R.id.discover_layout);
+        userCenterLayout = findViewById(R.id.userCenter_layout);
+        specialTopicImage = (ImageView) findViewById(R.id.specialTopic_image);
+        discoverImage = (ImageView) findViewById(R.id.discover_image);
+        userCenterImage = (ImageView) findViewById(R.id.userCenter_image);
+        specialTopicText = (TextView) findViewById(R.id.specialTopic_text);
+        discoverText = (TextView) findViewById(R.id.discover_text);
+        userCenterText = (TextView) findViewById(R.id.userCenter_text);
+        specialTopicLayout.setOnClickListener(this);
+        discoverLayout.setOnClickListener(this);
+        userCenterLayout.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.discoverGuidance_layout:
+            case R.id.specialTopic_layout:
                 // 当点击了消息tab时，选中第1个tab
                 setTabSelection(0);
                 break;
-            case R.id.discoverLabel_layout:
+            case R.id.discover_layout:
                 // 当点击了联系人tab时，选中第2个tab
                 setTabSelection(1);
                 break;
-            case R.id.discoverShare_layout:
+            case R.id.userCenter_layout:
                 // 当点击了设置tab时，选中第4个tab
                 setTabSelection(2);
                 break;
@@ -132,42 +132,42 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         switch (index) {
             case 0:
                 // 当点击了消息tab时，改变控件的图片和文字颜色
-                discoverGuidanceImage.setImageResource(R.drawable.tab_message_selected);
-                discoverGuidanceText.setTextColor(Color.WHITE);
-                if (discoverGuidanceFragment == null) {
-                    // 如果MessageFragment为空，则创建一个并添加到界面上
-                    discoverGuidanceFragment = new DiscoverGuidanceFragment();
-                    transaction.add(R.id.content, discoverGuidanceFragment);
+                specialTopicImage.setImageResource(R.drawable.tab_message_selected);
+                specialTopicText.setTextColor(Color.WHITE);
+                if (specialTopicFragment == null) {
+                    // 如果MamSpecialTopicFragment为空，则创建一个并添加到界面上
+                    specialTopicFragment = new MamSpecialTopicFragment();
+                    transaction.add(R.id.content, specialTopicFragment);
                 } else {
-                    // 如果MessageFragment不为空，则直接将它显示出来
-                    transaction.show(discoverGuidanceFragment);
+                    // 如果MamSpecialTopicFragment不为空，则直接将它显示出来
+                    transaction.show(specialTopicFragment);
                 }
                 break;
             case 1:
                 // 当点击了联系人tab时，改变控件的图片和文字颜色
-                discoverLabelImage.setImageResource(R.drawable.tab_contacts_selected);
-                discoverLabelText.setTextColor(Color.WHITE);
-                if (discoverLabelFragment == null) {
-                    // 如果ContactsFragment为空，则创建一个并添加到界面上
-                    discoverLabelFragment = new DiscoverLabelFragment();
-                    transaction.add(R.id.content, discoverLabelFragment);
+                discoverImage.setImageResource(R.drawable.tab_contacts_selected);
+                discoverText.setTextColor(Color.WHITE);
+                if (discoverFragment == null) {
+                    // 如果MamDiscoverFragment为空，则创建一个并添加到界面上
+                    discoverFragment = new MamDiscoverFragment();
+                    transaction.add(R.id.content, discoverFragment);
                 } else {
-                    // 如果ContactsFragment不为空，则直接将它显示出来
-                    transaction.show(discoverLabelFragment);
+                    // 如果MamDiscoverFragment不为空，则直接将它显示出来
+                    transaction.show(discoverFragment);
                 }
                 break;
             case 2:
             default:
                 // 当点击了设置tab时，改变控件的图片和文字颜色
-                discoverShareImage.setImageResource(R.drawable.tab_setting_selected);
-                discoverShareText.setTextColor(Color.WHITE);
-                if (discoverShareFragment == null) {
-                    // 如果SettingFragment为空，则创建一个并添加到界面上
-                    discoverShareFragment = new DiscoverShareFragment();
-                    transaction.add(R.id.content, discoverShareFragment);
+                userCenterImage.setImageResource(R.drawable.tab_setting_selected);
+                userCenterText.setTextColor(Color.WHITE);
+                if (userCenterFragment == null) {
+                    // 如果MamUserCenterFragment为空，则创建一个并添加到界面上
+                    userCenterFragment = new MamUserCenterFragment();
+                    transaction.add(R.id.content, userCenterFragment);
                 } else {
                     // 如果SettingFragment不为空，则直接将它显示出来
-                    transaction.show(discoverShareFragment);
+                    transaction.show(userCenterFragment);
                 }
                 break;
         }
@@ -178,12 +178,12 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
      * 清除掉所有的选中状态。
      */
     private void clearSelection() {
-        discoverGuidanceImage.setImageResource(R.drawable.tab_message_unselected);
-        discoverGuidanceText.setTextColor(Color.parseColor("#82858b"));
-        discoverLabelImage.setImageResource(R.drawable.tab_contacts_unselected);
-        discoverLabelText.setTextColor(Color.parseColor("#82858b"));
-        discoverShareImage.setImageResource(R.drawable.tab_setting_unselected);
-        discoverShareText.setTextColor(Color.parseColor("#82858b"));
+        specialTopicImage.setImageResource(R.drawable.tab_message_unselected);
+        specialTopicText.setTextColor(Color.parseColor("#82858b"));
+        discoverImage.setImageResource(R.drawable.tab_contacts_unselected);
+        discoverText.setTextColor(Color.parseColor("#82858b"));
+        userCenterImage.setImageResource(R.drawable.tab_setting_unselected);
+        userCenterText.setTextColor(Color.parseColor("#82858b"));
     }
 
     /**
@@ -192,14 +192,14 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
      * @param transaction 用于对Fragment执行操作的事务
      */
     private void hideFragments(FragmentTransaction transaction) {
-        if (discoverGuidanceFragment != null) {
-            transaction.hide(discoverGuidanceFragment);
+        if (specialTopicFragment != null) {
+            transaction.hide(specialTopicFragment);
         }
-        if (discoverLabelFragment != null) {
-            transaction.hide(discoverLabelFragment);
+        if (discoverFragment != null) {
+            transaction.hide(discoverFragment);
         }
-        if (discoverShareFragment != null) {
-            transaction.hide(discoverShareFragment);
+        if (userCenterFragment != null) {
+            transaction.hide(userCenterFragment);
         }
     }
 }
