0375a1660cec350fd8ef8140c2c2491ada0006ca
Michael <mrcoder@qq.com>
2015-06-24 23:28:16 +0800

    [Feature] HomeGroupActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/HomeSpecialTopicActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/HomeSpecialTopicActivity.java
@@ -1,13 +1,11 @@
 package com.babyspace.mamshare.app.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseActivity;
 
-public class HomeSpecialTopicActivity extends AppCompatActivity {
+public class HomeSpecialTopicActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
