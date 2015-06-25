9ad960087c98d723e2050615534c4c2a20329997
Michael <mrcoder@qq.com>
2015-06-25 17:43:29 +0800

    [Feature] delete MamStrollActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/HomeGroupActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/HomeGroupActivity.java
@@ -23,7 +23,7 @@ import butterknife.OnClick;
 import de.greenrobot.event.EventBus;
 
 public class HomeGroupActivity extends ActivityGroup {
-    //TODO 因为不可抗拒的原因 似乎只能弃用fragmentManager 而用ActivityGroup
+    //TODO 因为不可抗拒的原因 似乎只能弃用fragmentManager 而用ActivityGroup 最后又解决Fragment缓存的问题了 囧
     public static final int PAGE_SPECIAL_TOPIC = 1001;
     private int page = PAGE_SPECIAL_TOPIC;
     public static final int PAGE_DISCOVER = 1002;
