1a84e9a83036a3def0eb59ead842fdb291d8a49b
Michael <mrcoder@qq.com>
2015-06-26 10:32:30 +0800

    [Feature] Html5Activity

--- app/src/main/java/com/babyspace/mamshare/app/activity/Html5Activity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/Html5Activity.java
@@ -1,11 +1,11 @@
 package com.babyspace.mamshare.app.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseActivity;
 
-public class Html5Activity extends AppCompatActivity {
+public class Html5Activity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
