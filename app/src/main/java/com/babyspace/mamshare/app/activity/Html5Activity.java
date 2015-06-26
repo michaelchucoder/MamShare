b381b3cc2e7cfc0eed252f39a422f279a9586833
Michael <mrcoder@qq.com>
2015-06-26 10:32:08 +0800

    [Feature] Html5Activity

--- app/src/main/java/com/babyspace/mamshare/app/activity/Html5Activity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/Html5Activity.java
@@ -2,8 +2,6 @@ package com.babyspace.mamshare.app.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.babyspace.mamshare.R;
 
