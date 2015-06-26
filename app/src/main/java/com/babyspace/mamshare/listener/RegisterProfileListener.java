7420a00896057892b5fee15721f9c905af569eee
Michael <mrcoder@qq.com>
2015-06-26 23:47:42 +0800

    [Feature] RegisterProfile

---
+++ app/src/main/java/com/babyspace/mamshare/listener/RegisterProfileListener.java
@@ -0,0 +1,14 @@
+package com.babyspace.mamshare.listener;
+
+/**
+ * Created with Android Studio
+ * Package name: com.babyspace.mamshare
+ * Author: MichaelChuCoder
+ * Date: 2015/6/26
+ * Time: 23:43
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public interface RegisterProfileListener {
+    void onRegisterNameSelected();
+    void onRegisterRoleSelected();
+}
