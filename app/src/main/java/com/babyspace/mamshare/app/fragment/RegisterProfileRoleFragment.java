73828b7fb75d3ffe3679d9de4a48b91aa911fc81
Michael <mrcoder@qq.com>
2015-06-26 19:14:19 +0800

    [Feature] RegisterProfile

--- app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileRoleFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileRoleFragment.java
@@ -1,10 +1,6 @@
 package com.babyspace.mamshare.app.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.babyspace.mamshare.R;
 import com.babyspace.mamshare.basement.BaseFragment;
