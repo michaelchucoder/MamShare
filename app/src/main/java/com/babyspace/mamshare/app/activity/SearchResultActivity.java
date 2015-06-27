9538f193ba8e0bee513f67fd98b2ff855a491250
Michael <zhuxiaohui001@163.com>
2015-06-27 21:43:15 +0800

    [Feature] DiscoverSearchActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/SearchResultActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/SearchResultActivity.java
@@ -1,16 +1,55 @@
 package com.babyspace.mamshare.app.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.TextView;
 
 import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.adapter.TabPageAdapter;
+import com.babyspace.mamshare.app.fragment.SearchResultGuidanceFragment;
+import com.babyspace.mamshare.app.fragment.SearchResultSpecialTopicFragment;
 import com.babyspace.mamshare.basement.BaseActivity;
+import com.viewpagerindicator.TabPageIndicator;
+
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class SearchResultActivity extends BaseActivity{
+    public static final String[] TITLES = new String[]{"专题", "攻略"};
+    public static final Fragment[] FRAGMENTS = new Fragment[]{new SearchResultSpecialTopicFragment(), new SearchResultGuidanceFragment()};
+
+    private TabPageIndicator mIndicator;
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+
+    @InjectView(R.id.search_result_txt)
+    TextView search_result_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
+        mIndicator = (TabPageIndicator) findViewById(R.id.id_indicator);
+        mViewPager = (ViewPager) findViewById(R.id.id_pager);
+        mAdapter = new TabPageAdapter(getSupportFragmentManager(), TITLES, FRAGMENTS);
+        mViewPager.setAdapter(mAdapter);
+        mIndicator.setViewPager(mViewPager, 0);
+
+    }
+    @OnClick({R.id.search_result_txt})
+    public void doOnClick(View view) {
+        Intent i = new Intent();
+
+        switch (view.getId()) {
+            case R.id.search_result_txt:
+                break;
+
+        }
     }
 
 }
