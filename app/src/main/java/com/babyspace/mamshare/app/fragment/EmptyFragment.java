7c2e5aa4d505b4032957bc240e33b1b93ad03b98
Michael <zhuxiaohui001@163.com>
2015-06-27 21:51:30 +0800

    [Feature] EmptyFragment

--- app/src/main/java/com/babyspace/mamshare/app/fragment/EmptyFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/EmptyFragment.java
@@ -3,16 +3,14 @@ package com.babyspace.mamshare.app.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EmptyFragment extends Fragment {
+public class EmptyFragment extends BaseFragment {
 
 
     public EmptyFragment() {
@@ -21,10 +19,14 @@ public class EmptyFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empty, container, false);
+    public void init(Bundle savedInstanceState) {
+        setContentView(R.layout.fragment_empty);
+
+    }
+
+    @Override
+    public void initView() {
+
     }
 
 
