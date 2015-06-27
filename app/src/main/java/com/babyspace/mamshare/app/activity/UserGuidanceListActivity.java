991a8a395b13051e9079508a73859d938d548ab2
Michael <zhuxiaohui001@163.com>
2015-06-27 22:12:27 +0800

    [Feature] UserGuidanceListActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/UserGuidanceListActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/UserGuidanceListActivity.java
@@ -1,13 +1,11 @@
 package com.babyspace.mamshare.app.activity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseActivity;
 
-public class UserGuidanceListActivity extends ActionBarActivity {
+public class UserGuidanceListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +13,4 @@ public class UserGuidanceListActivity extends ActionBarActivity {
         setContentView(R.layout.activity_user_guidance_list);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user_guidance_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
