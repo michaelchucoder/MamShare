74128716d3139517010e2c907a4d51b4a232594d
Michael <mrcoder@qq.com>
2015-06-29 15:31:32 +0800

    [Feature] wheelpicker

--- app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileBabyFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileBabyFragment.java
@@ -2,22 +2,29 @@ package com.babyspace.mamshare.app.fragment;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.babyspace.mamshare.R;
 import com.babyspace.mamshare.app.activity.ChatActivity;
 import com.babyspace.mamshare.basement.BaseFragment;
+import com.michael.library.widget.wheelpicker.OnWheelChangedListener;
+import com.michael.library.widget.wheelpicker.WheelView;
+import com.michael.library.widget.wheelpicker.adapters.ArrayWheelAdapter;
+import com.michael.library.widget.wheelpicker.adapters.NumericWheelAdapter;
+
+import java.util.Calendar;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class RegisterProfileBabyFragment extends BaseFragment {
 
     @InjectView(R.id.btn_register_next)
@@ -26,6 +33,7 @@ public class RegisterProfileBabyFragment extends BaseFragment {
 
     public RegisterProfileBabyFragment() {
     }
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -44,6 +52,37 @@ public class RegisterProfileBabyFragment extends BaseFragment {
     @Override
     public void initView() {
 
+        Calendar calendar = Calendar.getInstance();
+
+        final WheelView month = (WheelView) findViewById(R.id.month);
+        final WheelView year = (WheelView) findViewById(R.id.year);
+        final WheelView day = (WheelView) findViewById(R.id.day);
+
+        OnWheelChangedListener listener = new OnWheelChangedListener() {
+            @Override
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                updateDays(year, month, day);
+            }
+        };
+
+        // month
+        int curMonth = calendar.get(Calendar.MONTH);
+        String months[] = new String[]{"January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October", "November", "December"};
+        month.setViewAdapter(new DateArrayAdapter(getActivity(), months, curMonth));
+        month.setCurrentItem(curMonth);
+        month.addChangingListener(listener);
+
+        // year
+        int curYear = calendar.get(Calendar.YEAR);
+        year.setViewAdapter(new DateNumericAdapter(getActivity(), curYear - 30, curYear, 0));
+        year.setCurrentItem(curYear);
+        year.addChangingListener(listener);
+
+        //day
+        updateDays(year, month, day);
+        day.setCurrentItem(calendar.get(Calendar.DAY_OF_MONTH) - 1);
+
     }
 
     @OnClick({R.id.btn_register_next})
@@ -59,5 +98,87 @@ public class RegisterProfileBabyFragment extends BaseFragment {
         }
     }
 
+
+    /**
+     * Updates day wheel. Sets max days according to selected month and year
+     */
+    void updateDays(WheelView year, WheelView month, WheelView day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) + year.getCurrentItem());
+        calendar.set(Calendar.MONTH, month.getCurrentItem());
+
+        int maxDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        day.setViewAdapter(new DateNumericAdapter(getActivity(), 1, maxDays, calendar.get(Calendar.DAY_OF_MONTH) - 1));
+        int curDay = Math.min(maxDays, day.getCurrentItem() + 1);
+        day.setCurrentItem(curDay - 1, true);
+    }
+
+    /**
+     * Adapter for numeric wheels. Highlights the current value.
+     */
+    private class DateNumericAdapter extends NumericWheelAdapter {
+        // Index of current item
+        int currentItem;
+        // Index of item to be highlighted
+        int currentValue;
+
+        /**
+         * Constructor
+         */
+        public DateNumericAdapter(Context context, int minValue, int maxValue, int current) {
+            super(context, minValue, maxValue);
+            this.currentValue = current;
+            setTextSize(16);
+        }
+
+        @Override
+        protected void configureTextView(TextView view) {
+            super.configureTextView(view);
+            if (currentItem == currentValue) {
+                view.setTextColor(0xFF0000F0);
+            }
+            view.setTypeface(Typeface.SANS_SERIF);
+        }
+
+        @Override
+        public View getItem(int index, View cachedView, ViewGroup parent) {
+            currentItem = index;
+            return super.getItem(index, cachedView, parent);
+        }
+    }
+
+    /**
+     * Adapter for string based wheel. Highlights the current value.
+     */
+    private class DateArrayAdapter extends ArrayWheelAdapter<String> {
+        // Index of current item
+        int currentItem;
+        // Index of item to be highlighted
+        int currentValue;
+
+        /**
+         * Constructor
+         */
+        public DateArrayAdapter(Context context, String[] items, int current) {
+            super(context, items);
+            this.currentValue = current;
+            setTextSize(16);
+        }
+
+        @Override
+        protected void configureTextView(TextView view) {
+            super.configureTextView(view);
+            if (currentItem == currentValue) {
+                view.setTextColor(0xFF0000F0);
+            }
+            view.setTypeface(Typeface.SANS_SERIF);
+        }
+
+        @Override
+        public View getItem(int index, View cachedView, ViewGroup parent) {
+            currentItem = index;
+            return super.getItem(index, cachedView, parent);
+        }
+    }
 
 }
