bb489a995f02f57b91b6e5ff66dbd88af0e627e7
Michael <mrcoder@qq.com>
2015-07-01 18:28:02 +0800

    [Feature] PickerDialogHelper

--- app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileBabyFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileBabyFragment.java
@@ -6,6 +6,7 @@ import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.ArrayMap;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import com.babyspace.mamshare.R;
 import com.babyspace.mamshare.app.activity.ChatActivity;
 import com.babyspace.mamshare.app.dialog.DialogHelper;
 import com.babyspace.mamshare.app.dialog.PickerDialog;
+import com.babyspace.mamshare.app.dialog.PickerDialogHelper;
 import com.babyspace.mamshare.app.dialog.ToastHelper;
 import com.babyspace.mamshare.basement.BaseFragment;
 import com.michael.library.widget.custom.DatePicker;
@@ -31,6 +33,8 @@ public class RegisterProfileBabyFragment extends BaseFragment {
     DatePicker datePicker;
 
     PickerDialog pickerDialog;
+
+    ArrayMap<String, String> expressTypes;
 
     public RegisterProfileBabyFragment() {
     }
@@ -75,7 +79,7 @@ public class RegisterProfileBabyFragment extends BaseFragment {
 
     }
 
-    @OnClick({R.id.btn_register_next, R.id.btn_register_day, R.id.btn_register_dialog})
+    @OnClick({R.id.btn_register_next, R.id.btn_register_day, R.id.btn_register_dialog, R.id.btn_register_picker})
     public void doOnClick(View view) {
 
         switch (view.getId()) {
@@ -105,6 +109,8 @@ public class RegisterProfileBabyFragment extends BaseFragment {
                 }).show();
 
                 break;
+            case R.id.btn_register_picker:
+
 
         }
     }
