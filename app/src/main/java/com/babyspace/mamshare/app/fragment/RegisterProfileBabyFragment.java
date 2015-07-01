8ea8d2066586ff5784e4596ac3654c82775799f0
Michael <mrcoder@qq.com>
2015-07-01 15:25:39 +0800

    [Feature] AppBaseTheme

--- app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileBabyFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterProfileBabyFragment.java
@@ -2,13 +2,10 @@ package com.babyspace.mamshare.app.fragment;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.babyspace.mamshare.R;
