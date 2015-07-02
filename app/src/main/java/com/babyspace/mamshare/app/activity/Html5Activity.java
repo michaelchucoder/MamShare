aae3dfa53c8b79aa0d8133da6e98b3ac5d3e32a5
Michael <zhuxiaohui001@163.com>
2015-07-02 23:05:19 +0800

    [Milestone] jpush

--- app/src/main/java/com/babyspace/mamshare/app/activity/Html5Activity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/Html5Activity.java
@@ -8,7 +8,6 @@ import com.babyspace.mamshare.basement.BaseActivity;
 public class Html5Activity extends BaseActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
