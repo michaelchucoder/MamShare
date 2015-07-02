89d0e2efac62681827ec39d2ecbcb5d5afc83e6d
Michael <mrcoder@qq.com>
2015-07-02 16:37:57 +0800

    [Feature] RecommendLabelActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/LabelEvaluateListActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/LabelEvaluateListActivity.java
@@ -1,16 +1,21 @@
 package com.babyspace.mamshare.app.activity;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.babyspace.mamshare.R;
 import com.babyspace.mamshare.basement.BaseActivity;
+import com.sina.weibo.sdk.api.share.Base;
 
-public class UserGuidanceListActivity extends BaseActivity {
+public class LabelEvaluateListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_guidance_list);
+        setContentView(R.layout.activity_label_evaluate_list);
     }
+
 
 }
