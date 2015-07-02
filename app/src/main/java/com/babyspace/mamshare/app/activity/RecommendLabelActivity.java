c70255b3918d4a9e2b4566a401fb9df2e521d39d
Michael <mrcoder@qq.com>
2015-07-02 17:17:18 +0800

    [Feature] RecommendLabelActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/RecommendLabelActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/RecommendLabelActivity.java
@@ -1,8 +1,7 @@
 package com.babyspace.mamshare.app.activity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 import com.babyspace.mamshare.R;
 import com.babyspace.mamshare.basement.BaseActivity;
@@ -15,25 +14,4 @@ public class RecommendLabelActivity extends BaseActivity {
         setContentView(R.layout.activity_recommend_label);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_recommend_label, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
