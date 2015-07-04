068aac1af02d19b8691872318b395ba4b09c1bed
Michael <mrcoder@qq.com>
2015-07-04 16:07:47 +0800

    [Feature] RegisterFragment

--- app/src/main/java/com/babyspace/mamshare/app/activity/LoginActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/LoginActivity.java
@@ -18,15 +18,6 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
-    @InjectView(R.id.edit_login_account)
-    EditText edit_login_account;
-
-    @InjectView(R.id.edit_login_pwd)
-    EditText edit_login_pwd;
-
-    @InjectView(R.id.btn_login_submit)
-    Button btn_login_submit;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,34 +42,4 @@ public class LoginActivity extends BaseActivity {
         startActivity(i);
     }
 
-
-
-    private void makeShapes() {
-
-        ShapeDrawable activeDrawable = new ShapeDrawable();
-        ShapeDrawable inactiveDrawable = new ShapeDrawable();
-        float mIndicatorSize=3f;
-        activeDrawable.setBounds(0, 0, (int) mIndicatorSize,
-                (int) mIndicatorSize);
-        inactiveDrawable.setBounds(0, 0, (int) mIndicatorSize,
-                (int) mIndicatorSize);
-
-        int i[] = new int[2];
-        i[0] = android.R.attr.textColorSecondary;
-        i[1] = android.R.attr.textColorSecondaryInverse;
-        TypedArray a = this.getTheme().obtainStyledAttributes(i);
-
-        Shape s1 = new OvalShape();
-        s1.resize(mIndicatorSize, mIndicatorSize);
-        Shape s2 = new OvalShape();
-        s2.resize(mIndicatorSize, mIndicatorSize);
-
-        ((ShapeDrawable) activeDrawable).getPaint().setColor(
-                a.getColor(0, Color.DKGRAY));
-        ((ShapeDrawable) inactiveDrawable).getPaint().setColor(
-                a.getColor(1, Color.LTGRAY));
-
-        ((ShapeDrawable) activeDrawable).setShape(s1);
-        ((ShapeDrawable) inactiveDrawable).setShape(s2);
-    }
 }
