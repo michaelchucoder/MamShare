324287e24abb19757762fcd9a951497c6c912b6b
Michael <mrcoder@qq.com>
2015-07-05 00:19:05 +0800

    [Feature] EvaluateDetailActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/EvaluateDetailActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/EvaluateDetailActivity.java
@@ -11,7 +11,7 @@ public class EvaluateDetailActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluate_detail);
-        //TODO
+        //TODO 动画效果的listview
     }
 
 }
