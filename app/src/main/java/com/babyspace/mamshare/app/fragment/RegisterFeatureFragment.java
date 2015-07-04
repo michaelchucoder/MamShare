aaa551ea5df30d8bf754a01e08fd6a55bd1ab5e4
Michael <mrcoder@qq.com>
2015-07-04 15:41:05 +0800

    [Feature] RegisterFragment

--- app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterFeatureFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterFeatureFragment.java
@@ -1,6 +1,7 @@
 package com.babyspace.mamshare.app.fragment;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,15 +10,33 @@ import android.view.ViewGroup;
 
 import com.babyspace.mamshare.R;
 import com.babyspace.mamshare.basement.BaseFragment;
+import com.babyspace.mamshare.listener.RegisterListener;
+
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFeatureFragment extends BaseFragment {
+    RegisterListener mCallback;
 
 
     public RegisterFeatureFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception.
+        try {
+            mCallback = (RegisterListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement RegisterProfileListener");
+        }
     }
 
 
@@ -31,11 +50,23 @@ public class RegisterFeatureFragment extends BaseFragment {
     @Override
     public void init(Bundle savedInstanceState) {
 
+        setContentView(R.layout.fragment_register_feature);
+
     }
 
     @Override
     public void initView() {
 
+    }
+
+    @OnClick({R.id.register_feature_custom})
+    public void doOnClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.register_feature_custom:
+                mCallback.onRegisterCustomSelected();
+                break;
+        }
     }
 
 
