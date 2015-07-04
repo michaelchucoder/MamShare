b1e9a2678fc07fcf5a9b3b998e3e812dddb43ca5
Michael <mrcoder@qq.com>
2015-07-04 11:54:07 +0800

    [Feature] GenericsAdapter

--- app/src/main/java/com/babyspace/mamshare/app/activity/HomePrefaceActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/HomePrefaceActivity.java
@@ -259,14 +259,15 @@ public class HomePrefaceActivity extends BaseActivity implements ViewPager.OnPag
             switch (pagePosition) {
                 case 0:
                     i.setClass(this, RecommendLabelActivity.class);
+                    this.overridePendingTransition(R.anim.umeng_fb_slide_in_from_left, R.anim.umeng_fb_slide_out_from_right);
+                    startActivity(i);
                     break;
                 case 1:
                     i.setClass(this, HomeUserCenterActivity.class);
+                    this.overridePendingTransition(R.anim.umeng_fb_slide_in_from_right, R.anim.umeng_fb_slide_out_from_left);
+                    startActivity(i);
                     break;
-
             }
-            startActivity(i);
-
         }
         lastState = state;
     }
