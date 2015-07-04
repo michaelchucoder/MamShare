108cb3166b3acd53dabac8272c93d8f3c46fb329
Michael <mrcoder@qq.com>
2015-07-04 09:15:30 +0800

    [Feature] GenericsAdapter

---
+++ app/src/main/java/com/babyspace/mamshare/adapter/GenericsAdapter.java
@@ -0,0 +1,12 @@
+package com.babyspace.mamshare.adapter;
+
+/**
+ * Created with Android Studio
+ * Package name: com.babyspace.mamshare.adapter
+ * Author: MichaelChuCoder
+ * Date: 2015-7-4
+ * Time: 9:15
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public class GenericsAdapter {
+}
