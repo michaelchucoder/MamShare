44ad34dd14b10310ecfaf7e1933c9406763f3862
Michael <zhuxiaohui001@163.com>
2015-07-05 16:40:17 +0800

    [Feature] CommonAdapter

--- app/src/main/java/com/babyspace/mamshare/adapter/MamaFeatureAdapter.java
+++ app/src/main/java/com/babyspace/mamshare/adapter/MamaFeatureAdapter.java
@@ -108,7 +108,7 @@ public class MamaFeatureAdapter extends BaseAdapter {
             holder.btn_avatar_1.setText(testBean2.isLike() ? "喜欢" : "无视");
             holder.btn_title_1.setText(testBean2.getTitle());
 
-            testBean3 = data.get(3 * (position) + 1);
+            testBean3 = data.get(3 * (position) + 2);
             holder.btn_avatar_1.setText(testBean3.isLike() ? "喜欢" : "无视");
             holder.btn_title_1.setText(testBean3.getTitle());
         }
