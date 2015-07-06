8e4beedf0d09b5165e00335472198dd0b9c7065b
Michael <mrcoder@qq.com>
2015-07-06 11:44:15 +0800

    [Feature] GenericsAdapter

--- app/src/main/java/com/babyspace/mamshare/app/fragment/UserCenterEvaluateFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/UserCenterEvaluateFragment.java
@@ -3,28 +3,28 @@ package com.babyspace.mamshare.app.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UserCenterEvaluateFragment extends Fragment {
+public class UserCenterEvaluateFragment extends BaseFragment {
 
 
     public UserCenterEvaluateFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void init(Bundle savedInstanceState) {
+        setContentView(R.layout.fragment_user_center_evaluate);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_center_evaluate, container, false);
+    public void initView() {
+
     }
 
 
