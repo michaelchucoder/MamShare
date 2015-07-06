a0a3e55a702f483b0cd4006d1bc92587de478c29
Michael <mrcoder@qq.com>
2015-07-06 12:01:19 +0800

    [Feature] GenericsAdapter

---
+++ app/src/main/java/com/babyspace/mamshare/adapter/AutoSearchAdapter.java
@@ -0,0 +1,76 @@
+package com.babyspace.mamshare.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.MamShare;
+import com.michael.core.tools.ViewRelayoutUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with Android Studio
+ * Package name: com.babyspace.mamshare.adapter
+ * Author: MichaelChuCoder
+ * Date: 2015-7-6
+ * Time: 11:56
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public class AutoSearchAdapter extends BaseAdapter {
+    private Context context;
+    private int resource = R.layout.item_auto_search;
+    private List<String> data = new ArrayList<String>();
+
+    public AutoSearchAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void resetData(List<String> newData) {
+        this.data.clear();
+        this.data.addAll(newData);
+        this.notifyDataSetChanged();
+    }
+
+    public void resetNullData() {
+        this.data.clear();
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return data.get(arg0);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        if (convertView == null) {
+            convertView = View.inflate(context, resource, null);
+            ViewRelayoutUtil.relayoutViewWithScale(convertView, MamShare.screenWidthScale);
+        }
+        String keyword = data.get(position);
+        convertView.setTag(keyword);
+        TextView txt_keyword = (TextView) convertView.findViewById(R.id.txt_keyword_ias);
+        txt_keyword.setText(keyword);
+        return convertView;
+    }
+
+}
--- app/src/main/java/com/babyspace/mamshare/commons/AppConstants.java
+++ app/src/main/java/com/babyspace/mamshare/commons/AppConstants.java
@@ -35,23 +35,25 @@ public class AppConstants {
      */
     public static final int page_home_guidance = 2001;
     public static final int page_home_evaluate = 2002;
-
     public static final int page_collect_guidance = 2003;
     public static final int page_collect_evaluate = 2004;
-
-
     public static final int page_discover_search = 2005;
-    public static final int item_discover_search = R.layout.item_discover_search;
-
     public static final int page_recommend_label = 2006;
-    public static final int item_recommend_label = R.layout.item_recommend_label;
-
     public static final int page_search_guidance = 2007;
     public static final int page_search_evaluate = 2008;
-    public static final int page_search_keyword = 2009;
+    public static final int page_label_evaluate = 2009;
+    public static final int page_user_evaluate = 2010;
 
-    public static final int page_label_evaluate = 2010;
-    public static final int page_user_evaluate = 2011;
+    public static final int item_home_guidance = R.layout.item_guidance;
+    public static final int item_home_evaluate = R.layout.item_evaluate;
+    public static final int item_collect_guidance = R.layout.item_guidance;
+    public static final int item_collect_evaluate = R.layout.item_evaluate;
+    public static final int item_discover_search = R.layout.item_discover_search;
+    public static final int item_recommend_label = R.layout.item_recommend_label;
+    public static final int item_search_guidance = R.layout.item_guidance;
+    public static final int item_search_evaluate = R.layout.item_evaluate;
+    public static final int item_label_evaluate = R.layout.item_evaluate;
+    public static final int item_user_evaluate = R.layout.item_evaluate;
 
 
 }
