3b0bbc602b0eccc34e12f15e4912e242386ba6da
Michael <mrcoder@qq.com>
2015-07-06 19:15:13 +0800

    [Milestone] ParallaxToolbarScrollViewActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/EvaluateDetailActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/EvaluateDetailActivity.java
@@ -1,17 +1,60 @@
 package com.babyspace.mamshare.app.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.babyspace.mamshare.R;
-import com.babyspace.mamshare.basement.BaseActivity;
+import com.michael.library.widget.ParallaxToolbar.BaseActivity;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ObservableScrollView;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ObservableScrollViewCallbacks;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ScrollState;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ScrollUtils;
+import com.nineoldandroids.view.ViewHelper;
 
-public class EvaluateDetailActivity extends BaseActivity {
+public class EvaluateDetailActivity extends BaseActivity implements ObservableScrollViewCallbacks {
+
+    private View mImageView;
+    private View mToolbarView;
+    private ObservableScrollView mScrollView;
+    private int mParallaxImageHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluate_detail);
-        //TODO 动画效果的listview  github上面的实例
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        mImageView = findViewById(R.id.image);
+        mToolbarView = findViewById(R.id.toolbar);
+        mToolbarView.setBackgroundColor(ScrollUtils.getColorWithAlpha(0, getResources().getColor(R.color.primary)));
+
+        mScrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        mScrollView.setScrollViewCallbacks(this);
+
+        mParallaxImageHeight = getResources().getDimensionPixelSize(R.dimen.parallax_image_height);
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        onScrollChanged(mScrollView.getCurrentScrollY(), false, false);
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        int baseColor = getResources().getColor(R.color.primary);
+        float alpha = Math.min(1, (float) scrollY / mParallaxImageHeight);
+        mToolbarView.setBackgroundColor(ScrollUtils.getColorWithAlpha(alpha, baseColor));
+        ViewHelper.setTranslationY(mImageView, scrollY / 2);
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
 }
