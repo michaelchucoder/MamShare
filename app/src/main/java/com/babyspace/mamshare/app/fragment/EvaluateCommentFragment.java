b31eb120d5aadf570be3b60cdec07d0e441384d8
Michael <mrcoder@qq.com>
2015-07-07 15:32:28 +0800

    [Milestone] ParallaxToolbarScrollViewActivity -web

---
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/EvaluateCommentFragment.java
@@ -0,0 +1,28 @@
+package com.babyspace.mamshare.app.fragment;
+
+
+import android.os.Bundle;
+import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseFragment;
+
+
+public class EvaluateCommentFragment extends BaseFragment {
+
+
+    public EvaluateCommentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+        setContentView(R.layout.fragment_evaluate_comment);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+
+}
