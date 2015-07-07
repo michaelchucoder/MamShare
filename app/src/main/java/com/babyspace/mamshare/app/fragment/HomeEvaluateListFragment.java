dbc0484c9ec3d629c25bf32eb22d2ed1037de7ac
Michael <mrcoder@qq.com>
2015-07-07 18:44:29 +0800

    [Milestone] ParallaxToolbarScrollViewActivity -web

--- app/src/main/java/com/babyspace/mamshare/app/fragment/HomeEvaluateListFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/HomeEvaluateListFragment.java
@@ -2,14 +2,11 @@ package com.babyspace.mamshare.app.fragment;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 
 import com.babyspace.mamshare.R;
 import com.babyspace.mamshare.basement.BaseFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class HomeEvaluateListFragment extends BaseFragment {
 
 
