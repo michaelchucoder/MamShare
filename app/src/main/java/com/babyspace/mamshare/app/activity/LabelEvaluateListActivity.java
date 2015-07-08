24539806454491ffd13c97edc515cb1ebddc2bb1
Michael <mrcoder@qq.com>
2015-07-08 16:30:05 +0800

    [Reformat] RecommendLabelActivity

--- app/src/main/java/com/babyspace/mamshare/app/activity/LabelEvaluateListActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/LabelEvaluateListActivity.java
@@ -1,17 +1,79 @@
 package com.babyspace.mamshare.app.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AbsListView;
 
 import com.babyspace.mamshare.R;
-import com.babyspace.mamshare.basement.BaseActivity;
+import com.michael.library.widget.ParallaxToolbar.BaseActivity;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ObservableGridView;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ObservableScrollViewCallbacks;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ScrollState;
+import com.michael.library.widget.ParallaxToolbar.observablescrollview.ScrollUtils;
+import com.nineoldandroids.view.ViewHelper;
 
-public class LabelEvaluateListActivity extends BaseActivity {
+public class LabelEvaluateListActivity extends BaseActivity implements ObservableScrollViewCallbacks {
+
+    private View mImageView;
+    private View mToolbarView;
+    private View mListBackgroundView;
+    private ObservableGridView mGridView;
+    private int mParallaxImageHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_label_evaluate_list);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        mImageView = findViewById(R.id.image);
+        mToolbarView = findViewById(R.id.toolbar);
+        mToolbarView.setBackgroundColor(ScrollUtils.getColorWithAlpha(0, getResources().getColor(R.color.primary)));
+
+        mParallaxImageHeight = getResources().getDimensionPixelSize(R.dimen.parallax_image_height);
+
+        mGridView = (ObservableGridView) findViewById(R.id.list);
+        mGridView.setScrollViewCallbacks(this);
+        // Set padding view for ListView. This is the flexible space.
+        View paddingView = new View(this);
+        AbsListView.LayoutParams lp = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,
+                mParallaxImageHeight);
+        paddingView.setLayoutParams(lp);
+
+        // This is required to disable header's list selector effect
+        paddingView.setClickable(true);
+
+        mGridView.addHeaderView(paddingView);
+        setDummyData(mGridView);
+
+        // mListBackgroundView makes ListView's background except header view.
+        mListBackgroundView = findViewById(R.id.list_background);
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        onScrollChanged(mGridView.getCurrentScrollY(), false, false);
+    }
 
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        int baseColor = getResources().getColor(R.color.primary);
+        float alpha = Math.min(1, (float) scrollY / mParallaxImageHeight);
+        mToolbarView.setBackgroundColor(ScrollUtils.getColorWithAlpha(alpha, baseColor));
+        ViewHelper.setTranslationY(mImageView, -scrollY / 2);
+
+        // Translate list background
+        ViewHelper.setTranslationY(mListBackgroundView, Math.max(0, -scrollY + mParallaxImageHeight));
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
 }
