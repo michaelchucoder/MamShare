4789c75fcce9910d11eb5c8aac579b59e31ca899
Michael <mrcoder@qq.com>
2015-07-09 19:04:53 +0800

    [Feature] GuidanceDetailActivity

--- app/src/main/java/com/babyspace/mamshare/app/fragment/GridViewEvaluateFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/GridViewEvaluateFragment.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.widget.AbsListView;
