d332bddb5cd931d9256248f91fe342c45ee54d59
Michael <mrcoder@qq.com>
2015-07-10 15:24:53 +0800

    [Feature] IntentParams

--- app/src/main/java/com/babyspace/mamshare/commons/IntentParams.java
+++ app/src/main/java/com/babyspace/mamshare/commons/IntentParams.java
@@ -3,7 +3,6 @@ package com.babyspace.mamshare.commons;
 public interface IntentParams {
 
     String prePage = "prePage";
-    String nextPage = "nextPage";
-    String currentPage = "currentPage";
+
 
 }
