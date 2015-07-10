3882a0702f5adb4f190f4f08c5edcfc8630b1fd1
Michael <mrcoder@qq.com>
2015-07-10 11:06:50 +0800

    [Feature] Bean and Event

---
+++ app/src/main/java/com/babyspace/mamshare/bean/EvaluateDetailEvent.java
@@ -0,0 +1,14 @@
+package com.babyspace.mamshare.bean;
+
+import com.babyspace.mamshare.basement.BaseResponseBean;
+
+/**
+ * Created with Android Studio
+ * Package name: com.babyspace.mamshare.bean
+ * Author: MichaelChuCoder
+ * Date: 2015-7-10
+ * Time: 11:04
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public class EvaluateDetailEvent extends BaseResponseBean<EvaluateDetail> {
+}
---
+++ app/src/main/java/com/babyspace/mamshare/bean/HomeEvaluateEvent.java
@@ -0,0 +1,16 @@
+package com.babyspace.mamshare.bean;
+
+import com.babyspace.mamshare.basement.BaseResponseBean;
+
+import java.util.List;
+
+/**
+ * Created with Android Studio
+ * Package name: com.babyspace.mamshare.bean
+ * Author: MichaelChuCoder
+ * Date: 2015-7-10
+ * Time: 11:03
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public class HomeEvaluateEvent extends BaseResponseBean<List<HomeEvaluateEvent>> {
+}
