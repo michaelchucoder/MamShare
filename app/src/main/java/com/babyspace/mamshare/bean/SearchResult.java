e7201994320c47fb160dfeec48c8db76e8a58fa4
Michael <mrcoder@qq.com>
2015-07-14 17:19:03 +0800

    [Feature] bean

--- app/src/main/java/com/babyspace/mamshare/bean/SearchResult.java
+++ app/src/main/java/com/babyspace/mamshare/bean/SearchResult.java
@@ -1,21 +1,24 @@
 package com.babyspace.mamshare.bean;
 
+
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.util.List;
+
 
 /**
  * Created with Android Studio
  * Package name: com.babyspace.mamshare.bean
  * Author: MichaelChuCoder
  * Date: 2015-7-14
- * Time: 16:57
+ * Time: 17:15
  * To change this template use File | Settings | File and Code Templates.
  */
-public class TagEvaluate implements Serializable {
-    @SerializedName("tag")
-    public String tag;
+public class SearchResult implements Serializable {
+
+    @SerializedName("strategy")
+    public List<Guidance> guidanceList;
     @SerializedName("eval")
-    public List<Evaluate> evaluates;
+    public List<Evaluate> evalList;
 }
---
+++ app/src/main/java/com/babyspace/mamshare/bean/SearchResultEvent.java
@@ -0,0 +1,14 @@
+package com.babyspace.mamshare.bean;
+
+import com.babyspace.mamshare.basement.BaseResponseBean;
+
+/**
+ * Created with Android Studio
+ * Package name: com.babyspace.mamshare.bean
+ * Author: MichaelChuCoder
+ * Date: 2015-7-14
+ * Time: 17:15
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public class SearchResultEvent extends BaseResponseBean<SearchResult> {
+}
