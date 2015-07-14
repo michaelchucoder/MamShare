ce2e6667f9c0a55e512b66129ca4172f789386f3
Michael <mrcoder@qq.com>
2015-07-14 17:14:00 +0800

    [Feature] bean

---
+++ app/src/main/java/com/babyspace/mamshare/bean/RecommendTagEvent.java
@@ -0,0 +1,17 @@
+package com.babyspace.mamshare.bean;
+
+
+import com.babyspace.mamshare.basement.BaseResponseBean;
+
+import java.util.List;
+
+/**
+ * Created with Android Studio
+ * Package name: com.babyspace.mamshare.bean
+ * Author: MichaelChuCoder
+ * Date: 2015-7-14
+ * Time: 17:12
+ * To change this template use File | Settings | File and Code Templates.
+ */
+public class RecommendTagEvent  extends BaseResponseBean<List<Tags>> {
+}
