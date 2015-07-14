1d0326eeddf3aa181a761ffa11e8013102c477a9
Michael <mrcoder@qq.com>
2015-07-14 09:29:26 +0800

    [Feature] GenericsAdapter

--- app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterPhoneFragment.java
+++ app/src/main/java/com/babyspace/mamshare/app/fragment/RegisterPhoneFragment.java
@@ -3,7 +3,6 @@ package com.babyspace.mamshare.app.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.View;
 
 import com.babyspace.mamshare.R;
