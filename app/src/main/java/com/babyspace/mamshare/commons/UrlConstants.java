cffa86670df1035d1e1dbd58f05e2966106875e4
Michael <mrcoder@qq.com>
2015-07-15 19:28:18 +0800

    [Feature] GenericsAdapter

--- app/src/main/java/com/babyspace/mamshare/commons/UrlConstants.java
+++ app/src/main/java/com/babyspace/mamshare/commons/UrlConstants.java
@@ -9,8 +9,27 @@ package com.babyspace.mamshare.commons;
  * To change this template use File | Settings | File and Code Templates.
  */
 public class UrlConstants {
+    /**
+     状态码
+     版本
+     描述
+     200	V1.1.1	成功
+     411	V1.1.1	没接到Header-mobileSN
+     400	V1.1.1	验签失败
+     401	V1.1.1	用户已存在
+     402	V1.1.1	手机验证码不正确
+     403	V1.1.1	登陆密码不正确
+     409	V1.1.1	旧密码不正确
+     404	V1.1.1	Token无效，需要重新登陆
+     500	V1.1.1
+     服务器内部错误
+     */
+    public static final int HTTP_NO_NETWORK = 404;
+    public static final String RESPONSE_OK = "200";
+    public static final String RESPONSE_No_header = "411";
+    public static final String RESPONSE_fail = "400";
+    public static final String RESPONSE_wrong_verify = "402";
 
-    //TODO
     /**
      * 妈妈说服务器地址
      */
@@ -18,15 +37,12 @@ public class UrlConstants {
     public static String BASE_WEB_URL = "http://t1api.m.0-6.com/appweb/apitest/testhtml5";
     public static String SERVER_URL = BASE_API_SERVER_URL + "/api/android";
 
-    /**
-     * All the URL are placed in the UrlConstants class
-     */
+
     public static final String VersionCheck = "/system/versionCheck";
     /**
      * 获取token
      */
     public static final String AccessToken = "/system/getEquipmentAccessToken";
-
     /**
      * 登录
      */
