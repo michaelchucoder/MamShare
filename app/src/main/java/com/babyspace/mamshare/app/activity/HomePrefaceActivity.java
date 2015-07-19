850613dd94a9371218a3a8e3e5687cc6ec0a3bf6
Michael <mrcoder@qq.com>
2015-07-19 23:31:11 +0800

    [UI] homePreface

--- app/src/main/java/com/babyspace/mamshare/app/activity/HomePrefaceActivity.java
+++ app/src/main/java/com/babyspace/mamshare/app/activity/HomePrefaceActivity.java
@@ -1,7 +1,6 @@
 package com.babyspace.mamshare.app.activity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
