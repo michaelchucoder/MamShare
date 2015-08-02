9b35ef6bae2847c4cdb4cc0134da5b8c52754d3e
Michael <mrcoder@qq.com>
2015-08-02 20:28:34 +0800

    [Feature] registerMamaRoleActivity

---
+++ app/src/main/java/com/babyspace/mamshare/app/activity/RegisterMamaRoleActivity.java
@@ -0,0 +1,19 @@
+package com.babyspace.mamshare.app.activity;
+
+import android.os.Bundle;
+
+import com.babyspace.mamshare.R;
+import com.babyspace.mamshare.basement.BaseActivity;
+
+/**
+ * Created by zhuo on 2015-7-29.
+ */
+public class RegisterMamaRoleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_register_role);
+    }
+}
